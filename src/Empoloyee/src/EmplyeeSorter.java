0929705c6398ce7ed2d9b5c37682556acde31251
MelnikovAN <sumkoprig@gmail.com>
2019-11-13 01:34:57 +0300

    HomeWork Employes  Problemm with universal comparator

---
+++ src/Empoloyee/src/EmplyeeSorter.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class EmplyeeSorter implements Comparator<Employee> {
+    public enum Order {Name,Company,Age,Sallary}
+    private Order sortingBy = Order.Name;
+
+    public void setSortingBy(Order sortingBy) {
+        this.sortingBy = sortingBy;
+    }
+
+    @Override
+    public int compare(Employee employee, Employee t1) {
+        switch (sortingBy){
+            case Name: return employee.getName().compareTo(t1.getName());
+            case Company: return employee.getCompany().compareTo(t1.getCompany());
+            case Age: return employee.getAge() - t1.getAge();
+            case Sallary: return employee.getSalary() -t1.getSalary();
+        }
+        return 0;
+    }
+}
+class EmpolyeSorterNum implements Comparator<Employee> {
+    public enum Order {Name, Company, Age, Sallary}
+
+    private ArrayList<Order> sortingbynum = new ArrayList();
+    private Order sort1 = Order.Name;
+    private Order sort2 = Order.Sallary;
+    private Order sort3 = Order.Age;
+    private Order sort4 = Order.Company;
+
+    public EmpolyeSorterNum(Order sort1, Order sort2, Order sort3, Order sort4) {
+        this.sort1 = sort1;
+        this.sort2 = sort2;
+        this.sort3 = sort3;
+        this.sort4 = sort4;
+        sortingbynum.add(sort1);
+        sortingbynum.add(sort2);
+        sortingbynum.add(sort3);
+        sortingbynum.add(sort4);
+        System.out.println("Алгоритм сортировки выглядит как :");
+        for (int i=0; i<sortingbynum.size(); i++){
+            System.out.println(sortingbynum.get(i));
+        }
+    }
+
+
+    @Override
+    public int compare(Employee employee, Employee t1) {
+        int value1 = 0;
+        switch (sortingbynum.get(0)) {
+            case Name:
+                value1 = employee.getName().compareTo(t1.getName());
+            case Company:
+                value1 = employee.getCompany().compareTo(t1.getCompany());
+            case Age:
+                value1 = employee.getAge() - t1.getAge();
+            case Sallary:
+                value1 = employee.getSalary() - t1.getSalary();
+        }
+        if (value1 == 0) {
+            int value2 = 0;
+            switch (sortingbynum.get(1)) {
+                case Name:
+                    value2 = employee.getName().compareTo(t1.getName());
+                case Company:
+                    value2 = employee.getCompany().compareTo(t1.getCompany());
+                case Age:
+                    value2 = employee.getAge() - t1.getAge();
+                case Sallary:
+                    value2 = employee.getSalary() - t1.getSalary();
+            }
+            if (value2 == 0) {
+                int value3 = 0;
+                switch (sortingbynum.get(2)) {
+                    case Name:
+                        value3 = employee.getName().compareTo(t1.getName());
+                    case Company:
+                        value3 = employee.getCompany().compareTo(t1.getCompany());
+                    case Age:
+                        value3 = employee.getAge() - t1.getAge();
+                    case Sallary:
+                        value3 = employee.getSalary() - t1.getSalary();
+                }
+                if (value3 == 0) {
+                    int value4 = 0;
+                    switch (sortingbynum.get(3)) {
+                        case Name:
+                            value4 = employee.getName().compareTo(t1.getName());
+                        case Company:
+                            value4 = employee.getCompany().compareTo(t1.getCompany());
+                        case Age:
+                            value4 = employee.getAge() - t1.getAge();
+                        case Sallary:
+                            value4 = employee.getSalary() - t1.getSalary();
+                    }
+                    return value4;
+                } else {
+                    return value3;
+                }
+            } else {
+                return value2;
+            }
+        } else {
+            return value1;
+        }
+    }
+}
---
+++ src/Empoloyee/src/main.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+        Employee markus = new Employee("Markus","Lego",2500,32);
+        System.out.println(markus);
+        List<Employee> emplis = Employee.employeeGenerator(8);
+       for (int i =0; i<emplis.size(); i++){
+           System.out.println(emplis.get(i));
+       }
+//       EmplyeeSorter byName = new EmplyeeSorter();
+//       byName.setSortingBy(EmplyeeSorter.Order.Name);
+//        Collections.sort(emplis,byName);
+//        System.out.println("After sort");
+//        for (int i =0; i<emplis.size(); i++){
+//            System.out.println(emplis.get(i));
+//        }
+//
+        EmpolyeSorterNum tr1 = new EmpolyeSorterNum(EmpolyeSorterNum.Order.Name, EmpolyeSorterNum.Order.Sallary, EmpolyeSorterNum.Order.Age, EmpolyeSorterNum.Order.Company);
+        Collections.sort(emplis,tr1);
+        System.out.println("After sort");
+        for (int i =0; i<emplis.size(); i++){
+            System.out.println(emplis.get(i));
+        }
+
+
+
+
+    }
+}
